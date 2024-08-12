f58584f5bf1fb89867caf9d90468b0d6df5d0c31
Irvan <dawammultazam99@gmail.com>
2024-08-12 11:42:15 +0700

    commit

--- models/product.go
+++ models/product.go
@@ -1,7 +1,7 @@
 package models
 
 type Product struct {
-	ID        int64  `gorm:"primary_key" json:"id"`
+	ID        int64  `gorm:"primary_key" json:"id"` 
 	Name      string `gorm:"type:varchar(300)" json:"name"`
 	Deskripsi string `gorm:"type:text" json:"deskripsi"`
 }
